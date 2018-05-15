70371c5434e1a55c1cf114b20f2b26d817360f85
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-05-15 20:42:38 +0900

    ban追加

--- tests/test_whitelist.py
+++ tests/test_whitelist.py
@@ -124,6 +124,27 @@ def test_changeInfo_error_1(web3,chain):
         whitelist_contract.transact().changeInfo(agent_address,encrypted_message_after)
 
 
+# エラー系2: 新規登録 -> BAN -> 更新
+def test_changeInfo_error_2(web3,chain):
+    account_address = web3.eth.accounts[0]
+    agent_address = web3.eth.accounts[1]
+
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    # 新規登録 -> Success
+    txn_hash_1 = whitelist_contract.transact().register(agent_address,encrypted_message)
+    chain.wait.for_receipt(txn_hash_1)
+
+    # BAN -> Success
+    web3.eth.defaultAccount = agent_address
+    txn_hash_2 = whitelist_contract.transact().ban(account_address)
+    chain.wait.for_receipt(txn_hash_2)
+
+    # 更新 -> Failure
+    with pytest.raises(TransactionFailed):
+        web3.eth.defaultAccount = account_address
+        whitelist_contract.transact().changeInfo(agent_address,encrypted_message_after)
+
 '''
 TEST3_支払情報を承認する(approve)
 '''
@@ -160,11 +181,13 @@ def test_approve_normal_1(web3,chain):
 # エラー系1: 登録なし -> 承認
 def test_approve_error_1(web3,chain):
     account_address = web3.eth.accounts[0]
+    agent_address = web3.eth.accounts[1]
 
     whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
 
     # 承認 -> Failure
     with pytest.raises(TransactionFailed):
+        web3.eth.defaultAccount = agent_address
         whitelist_contract.transact().approve(account_address)
 
 
@@ -183,7 +206,7 @@ def test_warn_normal_1(web3,chain):
     txn_hash_1 = whitelist_contract.transact().register(agent_address, encrypted_message)
     chain.wait.for_receipt(txn_hash_1)
 
-    # 承認 -> Success
+    # 警告 -> Success
     web3.eth.defaultAccount = agent_address
     txn_hash_2 = whitelist_contract.transact().warn(account_address)
     chain.wait.for_receipt(txn_hash_2)
@@ -204,11 +227,13 @@ def test_warn_normal_1(web3,chain):
 # エラー系1: 登録なし -> 警告
 def test_warn_error_1(web3,chain):
     account_address = web3.eth.accounts[0]
+    agent_address = web3.eth.accounts[1]
 
     whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
 
-    # 承認 -> Failure
+    # 警告 -> Failure
     with pytest.raises(TransactionFailed):
+        web3.eth.defaultAccount = agent_address
         whitelist_contract.transact().warn(account_address)
 
 
@@ -282,9 +307,56 @@ def test_unapprove_normal_2(web3,chain):
 # エラー系1: 登録なし -> 非承認
 def test_unapprove_error_1(web3,chain):
     account_address = web3.eth.accounts[0]
+    agent_address = web3.eth.accounts[1]
 
     whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
 
-    # 承認 -> Failure
+    # 非承認 -> Failure
     with pytest.raises(TransactionFailed):
+        web3.eth.defaultAccount = agent_address
         whitelist_contract.transact().unapprove(account_address)
+
+'''
+TEST5_支払情報をBAN状態にする(ban)
+'''
+
+# 正常系1: 新規登録 -> BAN
+def test_ban_normal_1(web3,chain):
+    account_address = web3.eth.accounts[0]
+    agent_address = web3.eth.accounts[1]
+
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    # 新規登録 -> Success
+    txn_hash_1 = whitelist_contract.transact().register(agent_address, encrypted_message)
+    chain.wait.for_receipt(txn_hash_1)
+
+    # BAN -> Success
+    web3.eth.defaultAccount = agent_address
+    txn_hash_2 = whitelist_contract.transact().ban(account_address)
+    chain.wait.for_receipt(txn_hash_2)
+
+    payment_account = whitelist_contract.call().payment_accounts(account_address, agent_address)
+    is_registered = whitelist_contract.call().isRegistered(account_address, agent_address)
+
+    # 登録情報の内容が正しいことを確認
+    assert payment_account[0] == account_address
+    assert payment_account[1] == agent_address
+    assert payment_account[2] == encrypted_message
+    assert payment_account[3] == 4
+
+    # 認可状態が未認可の状態であることを確認
+    assert is_registered == False
+
+
+# エラー系1: 登録なし -> BAN
+def test_ban_error_1(web3,chain):
+    account_address = web3.eth.accounts[0]
+    agent_address = web3.eth.accounts[1]
+
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    # BAN -> Failure
+    with pytest.raises(TransactionFailed):
+        web3.eth.defaultAccount = agent_address
+        whitelist_contract.transact().ban(account_address)
