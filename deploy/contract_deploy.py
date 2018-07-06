224d71e6fabf626f44209c00524e1839d1000b1c
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-07-06 20:54:22 +0900

    passwordを環境変数

--- deploy/contract_deploy.py
+++ deploy/contract_deploy.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import os
 from populus import Project
 
 
@@ -6,7 +7,7 @@ project = Project()
 with project.get_chain('dev_chain') as chain:
     web3 = chain.web3
     web3.eth.defaultAccount = web3.eth.accounts[0]
-    web3.personal.unlockAccount(web3.eth.defaultAccount, "password", 0)
+    web3.personal.unlockAccount(web3.eth.defaultAccount, os.environ.get('ETH_ACCOUNT_PASSWORD'), 0)
 
     token_list, _ = chain.provider.get_or_deploy_contract('TokenList')
     personal_info, _ = chain.provider.get_or_deploy_contract('PersonalInfo')
