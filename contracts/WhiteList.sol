19c79f637eb082bdb781678c85e1dfac262a86f8
YoshihitoAso <yoshihito.asoh@gmail.com>
2018-07-12 21:02:25 +0900

    決済業者の利用規約登録、同意機能を追加

--- contracts/WhiteList.sol
+++ contracts/WhiteList.sol
@@ -2,23 +2,38 @@ pragma solidity ^0.4.24;
 
 contract WhiteList {
 
+    // 利用規約同意
+    struct Agreement {
+        address account_address; // アカウントアドレス
+        address agent_address; // アカウントアドレス（決済業者）
+        bool status; // 同意状況（同意済:true）
+    }
+
+    // 利用規約
+    // agent_address => 規約本文
+    mapping(address => string) public terms;
+
+    // 利用規約同意情報
+    // account_address => agent_address => Agreement
+    mapping(address => mapping(address => Agreement)) public agreements;
+
     // 支払用口座
     struct PaymentAccount {
         address account_address; // アカウントアドレス
         address agent_address; // アカウントアドレス（決済業者）
         string encrypted_info; // 銀行口座情報（暗号化済）
-        uint8 approval_status; // 承認状態（NONE(0)/NG(1)/OK(2)/WARN(3)）
+        uint8 approval_status; // 承認状態（NONE(0)/NG(1)/OK(2)/WARN(3)/BAN(4)）
     }
 
     // 支払用口座情報
     // account_address => agent_address => PaymentAccount
     mapping(address => mapping(address => PaymentAccount)) public payment_accounts;
 
+    // イベント：規約同意
+    event Agree(address indexed account_address, address indexed agent_address);
+
     // イベント：登録
     event Register(address indexed account_address, address indexed agent_address);
-
-    // イベント：登録内容変更
-    event ChangeInfo(address indexed account_address, address indexed agent_address);
 
     // イベント：承認
     event Approve(address indexed account_address, address indexed agent_address);
@@ -36,31 +51,42 @@ contract WhiteList {
     constructor() public {
     }
 
+    // ファンクション：（決済業者）利用規約登録
+    function register_terms(string _text) public returns (bool) {
+        terms[msg.sender] = _text;
+        return true;
+    }
+
+    // ファンクション：利用規約同意
+    function agree_terms(address _account_address, address _agent_address)
+        private
+        returns (bool)
+    {
+        Agreement storage agreement = agreements[_account_address][_agent_address];
+        agreement.account_address = _account_address;
+        agreement.agent_address = _agent_address;
+        agreement.status = true;
+
+        emit Agree(_account_address, _agent_address);
+
+        return true;
+    }
+
     // ファンクション：（投資家）支払情報を登録する
+    //  ２回目以降は上書き登録を行う
     function register(address _agent_address, string _encrypted_info) public returns (bool) {
         PaymentAccount storage payment_account = payment_accounts[msg.sender][_agent_address];
-        require(payment_account.account_address == 0);
+        require(payment_account.approval_status != 4);
 
         payment_account.account_address = msg.sender;
         payment_account.agent_address = _agent_address;
         payment_account.encrypted_info = _encrypted_info;
         payment_account.approval_status = 1;
 
+        // 利用規約同意
+        agree_terms(msg.sender, _agent_address);
+
         emit Register(msg.sender, _agent_address);
-
-        return true;
-    }
-
-    // ファンクション：（投資家）支払情報の更新
-    function changeInfo(address _agent_address, string _encrypted_info) public returns (bool) {
-        PaymentAccount storage payment_account = payment_accounts[msg.sender][_agent_address];
-        require(payment_account.account_address != 0);
-        require(payment_account.approval_status != 4);
-
-        payment_account.encrypted_info = _encrypted_info;
-        payment_account.approval_status = 1;
-
-        emit ChangeInfo(msg.sender, _agent_address);
 
         return true;
     }
--- tests/test_whitelist.py
+++ tests/test_whitelist.py
@@ -3,11 +3,43 @@ from ethereum.tester import TransactionFailed
 
 encrypted_message = 'encrypted_message'
 encrypted_message_after = 'encrypted_message_after'
+terms_text = 'terms_sample\nend'
+
+'''
+TEST0_デプロイ
+'''
+# 正常系1: デプロイ
+def test_deploy_normal_1(web3, chain, users):
+    admin = users['admin']
+    trader = users['trader']
+    agent = users['agent']
+
+    # WhiteListデプロイ
+    web3.eth.defaultAccount = admin
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    payment_account = whitelist_contract.call().payment_accounts(trader, agent)
+    agreement = whitelist_contract.call().agreements(trader, agent)
+    is_registered = whitelist_contract.call().isRegistered(trader, agent)
+
+    # デフォルトの登録情報の内容が正しいことを確認
+    assert payment_account[0] == '0x0000000000000000000000000000000000000000'
+    assert payment_account[1] == '0x0000000000000000000000000000000000000000'
+    assert payment_account[2] == ''
+    assert payment_account[3] == 0
+
+    # デフォルトの規約同意情報の内容が正しいことを確認
+    assert agreement[0] == '0x0000000000000000000000000000000000000000'
+    assert agreement[1] == '0x0000000000000000000000000000000000000000'
+    assert agreement[2] == False
+
+    # 認可状態が未認可の状態であることを確認
+    assert is_registered == False
+
 
 '''
 TEST1_支払情報を登録（register）
 '''
-
 # 正常系1: 新規登録
 def test_register_normal_1(web3, chain, users):
     admin = users['admin']
@@ -24,6 +56,7 @@ def test_register_normal_1(web3, chain, users):
     chain.wait.for_receipt(txn_hash)
 
     payment_account = whitelist_contract.call().payment_accounts(trader, agent)
+    agreement = whitelist_contract.call().agreements(trader, agent)
     is_registered = whitelist_contract.call().isRegistered(trader, agent)
 
     # 登録情報の内容が正しいことを確認
@@ -32,9 +65,51 @@ def test_register_normal_1(web3, chain, users):
     assert payment_account[2] == encrypted_message
     assert payment_account[3] == 1
 
+    # 規約同意情報の内容が正しいことを確認
+    assert agreement[0] == trader
+    assert agreement[1] == agent
+    assert agreement[2] == True
+
     # 認可状態が未認可の状態であることを確認
     assert is_registered == False
 
+# 正常系2: 新規登録 -> 更新
+def test_register_normal_2(web3, chain, users):
+    admin = users['admin']
+    trader = users['trader']
+    agent = users['agent']
+
+    # WhiteListデプロイ
+    web3.eth.defaultAccount = admin
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    # 登録 -> Success
+    web3.eth.defaultAccount = trader
+    txn_hash_1 = whitelist_contract.transact().register(agent,encrypted_message)
+    chain.wait.for_receipt(txn_hash_1)
+
+    # 登録（２回目） -> Success
+    web3.eth.defaultAccount = trader
+    txn_hash_2 = whitelist_contract.transact().register(agent,encrypted_message_after)
+    chain.wait.for_receipt(txn_hash_2)
+
+    payment_account = whitelist_contract.call().payment_accounts(trader, agent)
+    agreement = whitelist_contract.call().agreements(trader, agent)
+    is_registered = whitelist_contract.call().isRegistered(trader, agent)
+
+    # 登録情報の内容が正しいことを確認
+    assert payment_account[0] == trader
+    assert payment_account[1] == agent
+    assert payment_account[2] == encrypted_message_after
+    assert payment_account[3] == 1
+
+    # 規約同意情報の内容が正しいことを確認
+    assert agreement[0] == trader
+    assert agreement[1] == agent
+    assert agreement[2] == True
+
+    # 認可状態が未認可の状態であることを確認
+    assert is_registered == False
 
 # エラー系1:入力値の型誤り（agent address）
 def test_register_error_1(web3, chain, users):
@@ -50,7 +125,6 @@ def test_register_error_1(web3, chain, users):
     web3.eth.defaultAccount = trader
     with pytest.raises(TypeError):
         whitelist_contract.transact().register(agent,encrypted_message)
-
 
 # エラー系2:入力値の型誤り（encrypted_info）
 def test_register_error_2(web3, chain, users):
@@ -68,137 +142,8 @@ def test_register_error_2(web3, chain, users):
     with pytest.raises(TypeError):
         whitelist_contract.transact().register(agent,encrypted_message)
 
-
-# エラー系3: 既に登録済みのアカウントの場合
+# エラー系3: 登録 -> BAN -> 登録（２回目）
 def test_register_error_3(web3, chain, users):
-    admin = users['admin']
-    trader = users['trader']
-    agent = users['agent']
-
-    # WhiteListデプロイ
-    web3.eth.defaultAccount = admin
-    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
-
-    # 新規登録 -> Success
-    web3.eth.defaultAccount = trader
-    txn_hash = whitelist_contract.transact().register(agent,encrypted_message)
-    chain.wait.for_receipt(txn_hash)
-
-    # 2回目登録 -> Failure
-    web3.eth.defaultAccount = trader
-    txn_hash = whitelist_contract.transact().register(agent,encrypted_message_after)
-    chain.wait.for_receipt(txn_hash)
-
-    payment_account = whitelist_contract.call().payment_accounts(trader, agent)
-    is_registered = whitelist_contract.call().isRegistered(trader, agent)
-
-    # 登録情報の内容が正しいことを確認
-    assert payment_account[0] == trader
-    assert payment_account[1] == agent
-    assert payment_account[2] == encrypted_message
-    assert payment_account[3] == 1
-
-    # 認可状態が未認可の状態であることを確認
-    assert is_registered == False
-
-
-'''
-TEST2_支払情報の更新(changeInfo)
-'''
-
-# 正常系1: 新規登録 -> 更新
-def test_changeInfo_normal_1(web3, chain, users):
-    admin = users['admin']
-    trader = users['trader']
-    agent = users['agent']
-
-    # WhiteListデプロイ
-    web3.eth.defaultAccount = admin
-    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
-
-    # 新規登録 -> Success
-    web3.eth.defaultAccount = trader
-    txn_hash_1 = whitelist_contract.transact().register(agent,encrypted_message)
-    chain.wait.for_receipt(txn_hash_1)
-
-    # 更新 -> Success
-    web3.eth.defaultAccount = trader
-    txn_hash_2 = whitelist_contract.transact().changeInfo(agent,encrypted_message_after)
-    chain.wait.for_receipt(txn_hash_2)
-
-    payment_account = whitelist_contract.call().payment_accounts(trader, agent)
-    is_registered = whitelist_contract.call().isRegistered(trader, agent)
-
-    # 登録情報の内容が正しいことを確認
-    assert payment_account[0] == trader
-    assert payment_account[1] == agent
-    assert payment_account[2] == encrypted_message_after
-    assert payment_account[3] == 1
-
-    # 認可状態が未認可の状態であることを確認
-    assert is_registered == False
-
-
-# 正常系2: 新規登録 -> 承認 -> 更新
-# 認可状態が未認可の状態に戻る
-def test_changeInfo_normal_2(web3, chain, users):
-    admin = users['admin']
-    trader = users['trader']
-    agent = users['agent']
-
-    # WhiteListデプロイ
-    web3.eth.defaultAccount = admin
-    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
-
-    # 新規登録 -> Success
-    web3.eth.defaultAccount = trader
-    txn_hash_1 = whitelist_contract.transact().register(agent,encrypted_message)
-    chain.wait.for_receipt(txn_hash_1)
-
-    # 承認 -> Success
-    web3.eth.defaultAccount = agent
-    txn_hash_2 = whitelist_contract.transact().approve(trader)
-    chain.wait.for_receipt(txn_hash_2)
-
-    # 更新 -> Success
-    web3.eth.defaultAccount = trader
-    txn_hash_3 = whitelist_contract.transact().changeInfo(agent,encrypted_message_after)
-    chain.wait.for_receipt(txn_hash_3)
-
-    payment_account = whitelist_contract.call().payment_accounts(trader, agent)
-    is_registered = whitelist_contract.call().isRegistered(trader, agent)
-
-    # 登録情報の内容が正しいことを確認
-    assert payment_account[0] == trader
-    assert payment_account[1] == agent
-    assert payment_account[2] == encrypted_message_after
-    assert payment_account[3] == 1
-
-    # 認可状態が未認可の状態であることを確認
-    assert is_registered == False
-
-
-# エラー系1: 登録なし -> 更新
-def test_changeInfo_error_1(web3, chain, users):
-    admin = users['admin']
-    trader = users['trader']
-    agent = users['agent']
-
-    # WhiteListデプロイ
-    web3.eth.defaultAccount = admin
-    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
-
-    # 更新 -> Success
-    web3.eth.defaultAccount = trader
-    txn_hash = whitelist_contract.transact().changeInfo(agent,encrypted_message_after)
-    chain.wait.for_receipt(txn_hash)
-
-    payment_account = whitelist_contract.call().payment_accounts(trader, agent)
-    assert payment_account[0] == '0x0000000000000000000000000000000000000000'
-
-
-# エラー系2: 新規登録 -> BAN -> 更新
-def test_changeInfo_error_2(web3, chain, users):
     admin = users['admin']
     trader = users['trader']
     agent = users['agent']
@@ -217,9 +162,9 @@ def test_changeInfo_error_2(web3, chain, users):
     txn_hash_2 = whitelist_contract.transact().ban(trader)
     chain.wait.for_receipt(txn_hash_2)
 
-    # 更新 -> Failure
+    # 登録（２回目） -> Failure
     web3.eth.defaultAccount = trader
-    txn_hash_3 = whitelist_contract.transact().changeInfo(agent,encrypted_message_after)
+    txn_hash_3 = whitelist_contract.transact().register(agent,encrypted_message_after)
     chain.wait.for_receipt(txn_hash_3)
 
     payment_account = whitelist_contract.call().payment_accounts(trader, agent)
@@ -236,9 +181,8 @@ def test_changeInfo_error_2(web3, chain, users):
 
 
 '''
-TEST3_支払情報を承認する(approve)
+TEST2_支払情報を承認する(approve)
 '''
-
 # 正常系1: 新規登録 -> 承認
 def test_approve_normal_1(web3, chain, users):
     admin = users['admin']
@@ -271,7 +215,6 @@ def test_approve_normal_1(web3, chain, users):
     # 認可状態が認可の状態であることを確認
     assert is_registered == True
 
-
 # エラー系1: 入力値の型誤り
 def test_approve_error_1(web3, chain, users):
     admin = users['admin']
@@ -286,7 +229,6 @@ def test_approve_error_1(web3, chain, users):
     web3.eth.defaultAccount = agent
     with pytest.raises(TypeError):
         whitelist_contract.transact().approve(trader)
-
 
 # エラー系2: 登録なし -> 承認
 def test_approve_error_2(web3, chain, users):
@@ -308,9 +250,8 @@ def test_approve_error_2(web3, chain, users):
 
 
 '''
-TEST4_支払情報を警告状態にする(warn)
+TEST3_支払情報を警告状態にする(warn)
 '''
-
 # 正常系1: 新規登録 -> 警告
 def test_warn_normal_1(web3, chain, users):
     admin = users['admin']
@@ -343,7 +284,6 @@ def test_warn_normal_1(web3, chain, users):
     # 認可状態が未認可の状態であることを確認
     assert is_registered == False
 
-
 # エラー系1: 入力値の型誤り
 def test_warn_error_1(web3, chain, users):
     admin = users['admin']
@@ -358,7 +298,6 @@ def test_warn_error_1(web3, chain, users):
     web3.eth.defaultAccount = agent
     with pytest.raises(TypeError):
         whitelist_contract.transact().warn(trader)
-
 
 # エラー系2: 登録なし -> 警告
 def test_warn_error_2(web3, chain, users):
@@ -380,9 +319,8 @@ def test_warn_error_2(web3, chain, users):
 
 
 '''
-TEST5_支払情報を非承認にする(unapprove)
+TEST4_支払情報を非承認にする(unapprove)
 '''
-
 # 正常系1: 新規登録 -> 非承認
 def test_unapprove_normal_1(web3, chain, users):
     admin = users['admin']
@@ -414,7 +352,6 @@ def test_unapprove_normal_1(web3, chain, users):
 
     # 認可状態が未認可の状態であることを確認
     assert is_registered == False
-
 
 # 正常系2: 新規登録 -> 承認 -> 非承認
 # 認可状態が未認可の状態に戻る
@@ -454,7 +391,6 @@ def test_unapprove_normal_2(web3, chain, users):
     # 認可状態が未認可の状態であることを確認
     assert is_registered == False
 
-
 # エラー系1: 入力値の型誤り
 def test_unapprove_error_1(web3, chain, users):
     admin = users['admin']
@@ -469,7 +405,6 @@ def test_unapprove_error_1(web3, chain, users):
     web3.eth.defaultAccount = agent
     with pytest.raises(TypeError):
         whitelist_contract.transact().unapprove(trader)
-
 
 # エラー系2: 登録なし -> 非承認
 def test_unapprove_error_2(web3, chain, users):
@@ -493,7 +428,6 @@ def test_unapprove_error_2(web3, chain, users):
 '''
 TEST5_支払情報をBAN状態にする(ban)
 '''
-
 # 正常系1: 新規登録 -> BAN
 def test_ban_normal_1(web3, chain, users):
     admin = users['admin']
@@ -526,7 +460,6 @@ def test_ban_normal_1(web3, chain, users):
     # 認可状態が未認可の状態であることを確認
     assert is_registered == False
 
-
 # エラー系1: 入力値の型誤り
 def test_ban_error_1(web3, chain, users):
     admin = users['admin']
@@ -541,7 +474,6 @@ def test_ban_error_1(web3, chain, users):
     web3.eth.defaultAccount = agent
     with pytest.raises(TypeError):
         whitelist_contract.transact().ban(trader)
-
 
 # エラー系2: 登録なし -> BAN
 def test_ban_error_2(web3, chain, users):
@@ -560,3 +492,39 @@ def test_ban_error_2(web3, chain, users):
     payment_account = whitelist_contract.call().payment_accounts(trader, agent)
 
     assert payment_account[0] == '0x0000000000000000000000000000000000000000'
+
+
+'''
+TEST6_利用規約登録（register_terms）
+'''
+# 正常系1: 新規登録
+def test_register_terms_normal_1(web3, chain, users):
+    admin = users['admin']
+    agent = users['agent']
+
+    # WhiteListデプロイ
+    web3.eth.defaultAccount = admin
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    # 利用規約登録
+    web3.eth.defaultAccount = agent
+    txn_hash = whitelist_contract.transact().register_terms(terms_text)
+    chain.wait.for_receipt(txn_hash)
+
+    terms = whitelist_contract.call().terms(agent)
+
+    assert terms == terms_text
+
+# エラー系1: 入力値の型誤り
+def test_register_terms_error_1(web3, chain, users):
+    admin = users['admin']
+    agent = users['agent']
+
+    # WhiteListデプロイ
+    web3.eth.defaultAccount = admin
+    whitelist_contract, _ = chain.provider.get_or_deploy_contract('WhiteList')
+
+    # 利用規約登録
+    web3.eth.defaultAccount = agent
+    with pytest.raises(TypeError):
+        whitelist_contract.transact().register_terms(1234)
