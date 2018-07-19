a1b5dbdba6892b39bd7317faf4a6fe827cd4d215
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-07-19 17:25:17 +0900

    戻す

--- deploy/contract_deploy.py
+++ deploy/contract_deploy.py
@@ -1,16 +1,13 @@
 # -*- coding:utf-8 -*-
 import os
 from populus import Project
-from web3 import Web3
-from web3.middleware import geth_poa_middleware
+
 
 project = Project()
 with project.get_chain('dev_chain') as chain:
     web3 = chain.web3
-    web3.middleware_stack.inject(geth_poa_middleware, layer=0)
     web3.eth.defaultAccount = web3.eth.accounts[0]
     web3.personal.unlockAccount(web3.eth.defaultAccount, os.environ.get('ETH_ACCOUNT_PASSWORD'), 0)
-
 
     token_list, _ = chain.provider.get_or_deploy_contract('TokenList')
     personal_info, _ = chain.provider.get_or_deploy_contract('PersonalInfo')
