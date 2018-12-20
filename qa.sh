c564f6d8aaccb8a8314fae10770e7d2135b40a60
Yohei Kitagawa <y-kitagawa@nri.co.jp>
2018-12-20 16:49:15 +0900

    カバレッジ取得

--- qa.sh
+++ qa.sh
@@ -7,4 +7,7 @@ cd /app/tmr-sc
 populus compile
 
 # test実施
-py.test tests/ --disable-pytest-warnings -v
+py.test tests/ --disable-pytest-warnings -v --cov=contracts/ --cov-report=xml --cov-branch
+
+# カバレッジファイルの移動
+mv coverage.xml cov/
