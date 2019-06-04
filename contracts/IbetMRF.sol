c8ffac16bc8055116c223a456452f2d9b8906c0e
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-06-04 18:30:46 +0900

    fix

--- contracts/IbetMRF.sol
+++ contracts/IbetMRF.sol
@@ -133,6 +133,7 @@ contract IbetMRF is Ownable, IbetStandardTokenInterface {
             balances[_to] = balanceOf(_to).add(_value);
         }
 
+        emit Transfer(_from, _to, _value);
         return true;
     }
 
