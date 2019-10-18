8a0950ef4ece2a1bff0c5dd91486c187b2b6294f
YoshihitoAso <yoshihito.asoh@gmail.com>
2019-10-18 19:58:05 +0900

    add Regulator Service

---
+++ contracts/RegulatorService.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.24;
+
+import "./Ownable.sol";
+
+/// @notice RegulatorServiceの標準インターフェース
+contract RegulatorService {
+
+    /*
+    * @notice 取引可否チェック
+    * @param _token トークンのアドレス
+    * @param _participant 取引参加者のアドレス（EOA）
+    * @return uint8 リターンコード：成功（0）
+    */
+    function check(address _token, address _participant)
+        public
+        returns (uint8);
+
+}
