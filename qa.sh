fbaabca3cbfd303b696c702eb5d2f89d0449b011
masuda-hi <masuda-hi@solidcom.co.jp>
2020-05-19 13:01:29 +0900

    fix: make GitHub Check fail when pytest fails.

--- qa.sh
+++ qa.sh
@@ -3,5 +3,21 @@ source ~/.bash_profile
 
 cd /app/ibet-SmartContract
 
+# コントラクトコードのコンパイル（キャッシュを使わずフルビルド）
+brownie compile --all
+
+TEST_LOG=$(mktemp)
+
 # test実施
-brownie test --disable-warnings -v --network quorum
+brownie test --disable-warnings -v --network quorum | tee "${TEST_LOG}"
+
+# `brownie test`のリターンコードはテスト結果に関わらず常に正常となるので、
+# コンソール出力内容からテスト結果を判定する
+PYTEST_ERRORS_HEADER="=========== ERRORS ==========="
+PYTEST_FAILURES_HEADER="=========== FAILURES ==========="
+if grep -e "${PYTEST_ERRORS_HEADER}" -e "${PYTEST_FAILURES_HEADER}" "${TEST_LOG}" > /dev/null; then
+  # pytest結果NG
+  exit 1
+fi
+
+rm "${TEST_LOG}"
