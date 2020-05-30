3e833b0156bcf6b13dfd23b371c474389f540ec8
YoshihitoAso <y-aso@boostry.co.jp>
2020-05-30 23:31:01 +0900

    feat: add natspec

--- contracts/ExchangeRegulatorService.sol
+++ contracts/ExchangeRegulatorService.sol
@@ -3,32 +3,35 @@ pragma solidity ^0.4.24;
 import "./Ownable.sol";
 import "./RegulatorService.sol";
 
+/// @title Exchangeの取引参加者制限サービス
+/// @dev Exchangeの取引参加者制限
 contract ExchangeRegulatorService is RegulatorService, Ownable {
 
-    // @dev 取引参加者の登録情報
+    /// 取引参加者の登録情報
     struct Participant {
-        address participant;
-        bool locked;
+        address participant; // 取引参加者EOAアドレス
+        bool locked; // ロック状態
     }
 
-    // @dev Check success code
+    /// Check success code
     uint8 constant private CHECK_SUCCESS = 0;
 
-    // @dev Check error reason : アカウントロック
+    /// Check error reason : アカウントロック
     uint8 constant private CHECK_LOCKED = 1;
 
-    // @dev Check error reason : アカウント未登録
+    /// Check error reason : アカウント未登録
     uint8 constant private CHECK_NOT_REGISTERD = 2;
 
-    // @dev 購入可能者情報
-    //      whitelist[participantAddress]
+    /// 購入可能者情報
+    /// whitelist[participantAddress]
     mapping(address => Participant) whitelist;
 
+    /// 参加者登録時イベント
     event Register(address indexed participant, bool locked);
 
-    /**
-     * @notice コントラクトアドレス判定
-     */
+    /// @dev コントラクトアドレス判定
+    /// @param _address アドレス
+    /// @return 判定結果
     function isContract(address _address)
         private
         view
@@ -41,20 +44,17 @@ contract ExchangeRegulatorService is RegulatorService, Ownable {
         return (length>0);
     }
 
-    /**
-     * @notice EOAアドレスチェック
-     * @param _address チェック対象のアドレス
-     */
+    /// @dev EOAアドレスチェック
+    /// @param _address チェック対象のアドレス
     modifier onlyEOA(address _address) {
         if (isContract(_address)) revert();
         _;
     }
 
-    /**
-     * @notice 取引参加者登録
-     * @param _participant 取引参加者のアドレス（EOA）
-     * @param _locked ロック状態
-     */
+    /// @notice 取引参加者登録
+    /// @dev コントラクトオーナーのみ実行可能
+    /// @param _participant 取引参加者のEOAアドレス
+    /// @param _locked ロック状態
     function register(address _participant, bool _locked)
         public
         onlyOwner()
@@ -65,10 +65,10 @@ contract ExchangeRegulatorService is RegulatorService, Ownable {
         emit Register(_participant, _locked);
     }
 
-    /**
-     * @notice 取引参加者参照
-     * @param _participant 取引参加者のアドレス（EOA）
-     */
+    /// @notice 取引参加者参照
+    /// @param _participant 取引参加者のEOAアドレス
+    /// @return participant 取引参加者のEOAアドレス
+    /// @return ロック状態
     function getWhitelist(address _participant)
         public
         view
@@ -78,10 +78,9 @@ contract ExchangeRegulatorService is RegulatorService, Ownable {
         locked = whitelist[_participant].locked;
     }
 
-    /**
-     * @notice 取引可否チェック
-     * @param _participant 取引参加者のアドレス（EOA）
-     */
+    /// @notice 取引可否チェック
+    /// @param _participant 取引参加者のアドレス（EOA）
+    /// @return チェック結果
     function check(address _participant)
         public
         view
