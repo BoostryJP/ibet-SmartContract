d03ab6a292cbc6a6453e8a9b60fc046b1b90bdb0
YoshihitoAso <y-aso@boostry.co.jp>
2020-11-08 02:41:22 +0900

    feat: add copyright

--- scripts/PaymentGateway_addAgent.py
+++ scripts/PaymentGateway_addAgent.py
@@ -1,17 +1,33 @@
-# -*- coding:utf-8 -*-
 """
-収納代行業者（Agent）アドレス登録
-・認可されたAgentアドレスをPaymentGatewayコントラクトに登録する。
+Copyright BOOSTRY Co., Ltd.
 
-Parameters
-----------
-contract_address : str
-    PaymentGatewayコントラクトのアドレス。
-    デプロイ済のものを指定する。
-agent_address : str
-    収納代行業者のアカウントアドレス。
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
 
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed onan "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
 """
+
+# 収納代行業者（Agent）アドレス登録
+# ・認可されたAgentアドレスをPaymentGatewayコントラクトに登録する。
+#
+# Parameters
+# ----------
+# contract_address : str
+#     PaymentGatewayコントラクトのアドレス。
+#     デプロイ済のものを指定する。
+# agent_address : str
+#     収納代行業者のアカウントアドレス。
+
 import os
 
 import sys
